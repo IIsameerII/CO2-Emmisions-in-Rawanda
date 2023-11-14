3ce3ece1249b4decfdcae79414919ab6d190773f
IIsameerII <sameerahamed300@gmail.com>
2023-11-13 22:13:33 -0330

    Streamlit building

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Setting the title of the page
+st.title('Prediction of CO2 Emmisisons in Rawanda')
+
+# Setting the contributor name
+st.subheader('Contributors \n * Sameer Ahamed \n * Peter Utomakili \n * Adejoke Adeoye')
+
+# Project Overview
+st.subheader('Project Overview')
+st.write('With a primary focus on greenhouse gas emissions, particularly carbon dioxide (CO2). These emissions result from a variety of human activities, including the use of fossil fuels for energy, transportation, manufacturing, and deforestation. Rising global temperatures, more frequent and severe extreme weather events, the melting of the polar ice caps, an increase in sea level, and disruptions to ecosystems and biodiversity are some of the negative repercussions that follow. To ensure a healthy and habitable planet for both the present and future generations, addressing the emissions problem is a global necessity that requires cooperative efforts to reduce and ultimately eradicate these harmful emissions.')
